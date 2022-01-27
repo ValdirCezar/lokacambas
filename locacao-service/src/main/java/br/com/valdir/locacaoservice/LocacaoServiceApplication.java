f9d8fa6d6b088e85455244ca48d5fd5931669d81
Valdir Cezar <vjesus@alelo.com.br>
2022-01-27 11:17:16 -0300

    chore: start locacao service

---
+++ locacao-service/src/main/java/br/com/valdir/locacaoservice/LocacaoServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.valdir.locacaoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocacaoServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocacaoServiceApplication.class, args);
+    }
+
+}
---
+++ locacao-service/src/test/java/br/com/valdir/locacaoservice/LocacaoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.valdir.locacaoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocacaoServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

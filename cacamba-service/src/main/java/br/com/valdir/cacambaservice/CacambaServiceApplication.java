d28cf3df1fb08217f0d2c37b6470dce9eb7b30d1
Valdir Cezar <vjesus@alelo.com.br>
2022-01-27 11:15:13 -0300

    chore: start cacamba service

---
+++ cacamba-service/src/main/java/br/com/valdir/cacambaservice/CacambaServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.valdir.cacambaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacambaServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CacambaServiceApplication.class, args);
+    }
+
+}

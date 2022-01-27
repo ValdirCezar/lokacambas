7e5b65883e4bfa3f244aa14d39e929b10ee70364
Valdir Cezar <vjesus@alelo.com.br>
2022-01-27 11:11:58 -0300

    chore: start user service

---
+++ user-service/src/test/java/br/com/valdir/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.valdir.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

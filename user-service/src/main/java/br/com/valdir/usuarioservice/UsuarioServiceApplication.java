25fafb2c433deb86dfad7d1ecca95d155d0a2afb
Valdir Cezar <vjesus@alelo.com.br>
2022-01-27 13:08:06 -0300

    refact: rename principal class

--- user-service/src/main/java/br/com/valdir/usuarioservice/UsuarioServiceApplication.java
+++ user-service/src/main/java/br/com/valdir/usuarioservice/UsuarioServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UserServiceApplication {
+public class UsuarioServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(UserServiceApplication.class, args);
+        SpringApplication.run(UsuarioServiceApplication.class, args);
     }
 
 }
